1c43e51e8fb1c8c7ede7fcf51dc6512c3c59c60d
Tichyus <quentin-maillard@orange.fr>
2021-02-10 17:50:06 +0100

    starting creating MVC architecture

--- main.go
+++ main.go
@@ -1,39 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"gorm.io/gorm"
 	"gorm.io/driver/mysql"
-  )
-  
-  type User struct {
-	gorm.Model
-	Username	string
-	Password	uint
-	Articles	[]Article
-	Comments 	[]Comment
-  }
-
-  type Article struct {
-	gorm.Model
-	UserId		uint
-	User		User
-	Title		string
-	Content		string
-	Comments 	[]Comment
-  }
-
-  type Comment struct {
-	gorm.Model
-	UserId		uint
-	User		User
-	ArticleId	uint
-	Article		Article
-	Content		string
-  }
+)
 
 func main() {
-	// connect database
+	// connect to database
 	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
