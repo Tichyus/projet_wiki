f2ae273c0d9e8693d237c750246bb02afb26ca01
Lucas Lehot <lehotlucas@gmail.com>
2021-02-10 15:58:45 +0100

    first shot on model creation and db connection

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/lucaslehot/projet_wiki
 go 1.15
 
 require (
-	gorm.io/driver/mysql v1.0.4 // indirect
-	gorm.io/gorm v1.20.12 // indirect
+	gorm.io/driver/mysql v1.0.4
+	gorm.io/driver/sqlite v1.1.4
+	gorm.io/gorm v1.20.12
 )
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"gorm.io/driver/mysql"
+  )
+  
+  type User struct {
+	gorm.Model
+	Username	string
+	Password	uint
+	Posts		[]Post
+	Comments 	[]Comment
+  }
+
+  type Post struct {
+	gorm.Model
+	UserId		uint
+	User		User
+	Title		string
+	Content		string
+	Comments 	[]Comment
+  }
+
+  type Comment struct {
+	gorm.Model
+	UserId		uint
+	User		User
+	PostId		uint
+	Post		Post
+	Content		string
+  }
 
 func main() {
-	fmt.Println("Hello, world.")
+	// connect database
+	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
